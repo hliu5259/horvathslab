09010ff684261ecab0b4355ca4c4481adf5ff212
pnm27 <47644233+pnm27@users.noreply.github.com>
2020-11-23 18:44:32 -0500

    Corrected typos, added comments, added appropriate brackets

--- seurat1-2.R
+++ seurat1-2.R
@@ -3,24 +3,24 @@
 # filter the Seurat datset based on the feature distribution(saved in the '_beforefilter.rds')
 
 # load package
-print('loading required package data.table, tidyverse, Seurat, SingleR')
+print('loading required packages: data.table, tidyverse, Seurat, SingleR')
 suppressMessages(library(data.table, quietly = TRUE))
 suppressMessages(library(tidyverse, quietly = TRUE))
 suppressMessages(library(Seurat, quietly = TRUE))
 suppressMessages(library(SingleR, quietly = TRUE))
 
-#setup graph environment for mgpc
-options(bitmapType="cairo")
+#setup graph environment for Unix-based system
+# options(bitmapType="cairo")
 
 # manages command line arguments
 handle_command_args <- function(args) {
   # make sure all flags are paired
-  if(length(args) %%2 != 0) stop("Command line arguments supplied incorrectly!")
+  if(length(args) %%2 != 0) stop("Command line arguments aren't paired. Note that arguments are always represented as key-value pairs!")
   
   # load the flags into a "dictionary"
   arg_df <- data.frame(cbind(flag = args[seq(1, length(args), by = 2)], value = args[seq(2, length(args), by = 2)])) %>% mutate_all(as.character)
     
-  # imput sampel list and min feature, max feature, number of mito gene contians
+  # input sample list, min feature, max feature, max percent of mitochondrial genes
   sample <<- arg_df$value[arg_df$flag == "-s"]
   sample_list <<- fread(sample, header = F)
   sample_id <<- sample_list$V1
@@ -29,12 +29,12 @@ handle_command_args <- function(args) {
   n_percent <- as.numeric(sample_list$V4)
   
   # check the argument
-  if (nrow(sample_list) ==0) stop("sample id supplied incorrectly")
-  if (ncol(sample_list) < 4) stop("feature argment applied incorrectly")
+  if (nrow(sample_list) == 0) stop("sample id supplied incorrectly")
+  if (ncol(sample_list) < 4) stop("feature argument applied incorrectly")
   
   # check the import dataset
   for (i in 1:nrow(sample_list)){
-    if (file.exists(paste0(sample_id[i], "_beforefilter.rds") == F)
+    if (file.exists(paste0(sample_id[i], "_beforefilter.rds") == F))
             print(paste0(sample_id[i], "_beforefilter.rds NOT exist"))
     else print(paste0("gonna process ", sample_id[i]))
   }
@@ -52,6 +52,7 @@ for (i in 1:nrow(sample_list)){
     if (!dir.exists(sample_id[i])) {
       cat('Creating output directory...\n')
       dir.create(sample_id[i], showWarnings = FALSE)
+    }
       
     # filter unique feature
     Gene_Seurat <- subset(x = Gene_Seurat, subset = nFeature_RNA >nfeature_min[i] & nFeature_RNA <nfeature_max[i] & percent.mt <n_percent[i] )
@@ -70,14 +71,14 @@ for (i in 1:nrow(sample_list)){
     # save filterd Seurat rds file
     saveRDS(Gene_Seurat,paste0(sample_id[i],"_filtered.rds"))
     
-    # remove the orgrial rds file to release more space
+    # remove rds file produced before filtering
     file.remove(paste0(sample_id[i]), "_beforefilter.rds")
     
-    # normatlizaion and scale
+    # normatlization and scale
     Gene_Seurat <- SCTransform(object = Gene_Seurat, vars.to.regress = "percent.mt", verbose = FALSE, variable.features.n = 6000)
     nor_gene_matrix <- as.data.frame(Gene_Seurat@assays[["RNA"]]@data)
     
-    # build up the normatlized gene expression mattrix
+    # build the normatlized gene expression mattrix
     fwrite(nor_gene_matrix, paste0(sample_id[i],"_GE_matrix_filtered.txt"), sep = '\t', quote = F, row.names = T)
 
     # PCA and cluster
@@ -88,7 +89,7 @@ for (i in 1:nrow(sample_list)){
     print(ElbowPlot(object = Gene_Seurat))
     dev.off()
     
-    # use UMAP as visualization methods
+    # use UMAP as visualization method
     Gene_Seurat<- RunUMAP(Gene_Seurat, dims = 1:20)
     Gene_Seurat <- FindNeighbors(Gene_Seurat, verbose = FALSE, dims = 1:20)
     Gene_Seurat <- FindClusters(Gene_Seurat, verbose = FALSE, resolution = 0.2)
@@ -126,4 +127,3 @@ for (i in 1:nrow(sample_list)){
 
 
 }
-#import featureCount gene_expression matrix
